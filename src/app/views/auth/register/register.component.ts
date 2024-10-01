c82647943f179871cc1b0da8a451c58426ed1e46
Silas Owu <owusilasyp00@gmail.com>
2024-10-01 03:17:15 -0700

    🎨 Style: add ui for register page with routing to login page

--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
