3edbdf1589ef2a6f25ae224ef14e921bed7d6cd9
Silas Owu <owusilasyp00@gmail.com>
2024-10-01 00:17:38 -0700

    🚚 "Chore: setup default route and routes for auth " pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './views/auth/auth/auth.component';
+import { LoginComponent } from './views/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent},
+      {
+        path: 'register',
+        loadComponent: () => import('./views/auth/register/register.component')
+        .then(mod => mod.RegisterComponent)
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./views/auth/user-profile/user-profile.component')
+        .then(mod => mod.UserProfileComponent)
+      }
+    ],
+  }
+];
