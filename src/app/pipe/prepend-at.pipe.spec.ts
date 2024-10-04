b0fef5e74251feab727a4547c1846eb1270a1d74
Silas Owu <owusilasyp00@gmail.com>
2024-10-03 17:24:46 -0700

    add custom pipe to prepend @ symbol

---
+++ src/app/pipe/prepend-at.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PrependAtPipe } from './prepend-at.pipe';
+
+describe('PrependAtPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PrependAtPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
